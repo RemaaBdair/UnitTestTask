997b8cb5051305f651a1cd43cca81bba02a57030
RemaaBdair <RemaaBdair@users.noreply.github.com>
2020-03-12 16:10:39 +0200

    new test case at line 35

--- test/filter.test.ts
+++ test/filter.test.ts
@@ -32,6 +32,18 @@ describe("filter", () => {
     };
     expect(filterRowss([col1, col2], fv)).toStrictEqual([col1, col2]);
   });
+  test("should expect [] when filter1By= is equal to and filter2by= is equal to and compareValue is And with different filterValues", () => {
+    let fv: FilterFormValues = {
+      filter1By: "Is equal to",
+      filter1Value: "red",
+      filter2By: "Is equal to",
+      filter2Value: "blue",
+      compareValue: "And",
+    column:'id'
+      };
+    expect(filterRowss([col1, col2], fv)).toStrictEqual([]);
+  });
+  
 });
 
 

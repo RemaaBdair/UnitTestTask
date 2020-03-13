29fa1803a0f9a54fa65fa3593a53d0bc70f180e6
RemaaBdair <RemaaBdair@users.noreply.github.com>
2020-03-13 17:56:53 +0200

    add new test case at line 541

--- test/filter.test.ts
+++ test/filter.test.ts
@@ -538,6 +538,17 @@ describe("filter", () => {
       };
     expect(filterRowss([col1, col2], fv)).toStrictEqual([col1]);
   });/**+1 */
+  test("should expect [] when filter1By= Contains and filter2by= is not equal to and compareValue is And", () => {
+    let fv: FilterFormValues = {
+      filter1By: "Contains",
+      filter1Value: "r",
+      filter2By: "Is not equal to",
+      filter2Value: "red",
+      compareValue: "And",
+    column:'id'
+      };
+    expect(filterRowss([col1, col2], fv)).toStrictEqual([]);
+  });
 });
 
 

70928d11e5b5f09cd50aa8b03fb23a8ecb8508a7
RemaaBdair <RemaaBdair@users.noreply.github.com>
2020-03-14 04:50:32 +0200

    filterRows is modefied to filter by nested columns

--- filter.ts
+++ filter.ts
@@ -1,16 +1,12 @@
-type FilterOptions =
-  | "Is equal to"
-  | "Is not equal to"
-  | "Starts with"
-  | "Contains"
-  | "Does not contain"
+type FilterOptions = "Is equal to" | "Is not equal to"
+  | "Starts with" | "Contains" | "Does not contain"
   | "Ends with";
 
 type CompareOptions = "And" | "Or";
 interface Column {
-  id: string;
-  category: string;
-  name: string
+  id: string |Record<string,string>;
+  category: string |Record<string,string>;
+  name: string | Record<string,string>
 }
 type FilterFormValues = {
   filter1By: FilterOptions;
@@ -18,97 +14,116 @@ type FilterFormValues = {
   filter2By: FilterOptions;
   filter2Value: string;
   compareValue: CompareOptions;
-  column?: keyof Column;
+  column?: keyof Column | ((elem: Column) => string);
 };
 
-function op(b1:boolean, operator, b2:boolean) :boolean{
+function op(b1: boolean, operator, b2: boolean): boolean {
   return operator === "And" ? b1 && b2 : b1 || b2;
 }
-const filterRows = (
-  rows: Column[],
-  filterValues: FilterFormValues
-): Column[] => {
-  let rowsToFilter = rows;
-  let column: keyof Column = filterValues.column;
 
+function propf(prop, row) {
+  if (typeof prop === "function") return prop(row);
+  else return row[prop];
+}
+const filterRows = (rows: Column[],filterValues: FilterFormValues): Column[] => {
+  let rowsToFilter = rows;
+  let prop: keyof Column | ((elem) => string) = filterValues.column;
   if (!filterValues.filter1Value) {
     return rowsToFilter;
   }
-  const operand2 = (filterBy:string, row:Column,op:string):boolean => {
+  const operand2 = (
+    filterBy: FilterOptions,
+    row: Column,
+    op: string
+  ): boolean => {
     let values = {
       "Is equal to": function(row) {
         return filterValues.filter2Value
-          ? row[column].toLowerCase() ===
+          ? propf(prop, row).toLowerCase() ===
               filterValues.filter2Value.toLowerCase()
-          : (op==="And"?true :false);
+          : op === "And"
+          ? true
+          : false;
       },
       "Is not equal to": function(row) {
         return filterValues.filter2Value
-          ? row[column].toLowerCase() !==
+          ? propf(prop, row).toLowerCase() !==
               filterValues.filter2Value.toLowerCase()
-          : op==="And"?true :false;
+          : op === "And"
+          ? true
+          : false;
       },
       "Starts with": function(row) {
         return filterValues.filter2Value
-          ? row[column]
+          ? propf(prop, row)
               .toLowerCase()
               .startsWith(filterValues.filter2Value.toLowerCase())
-          : op==="And"?true :false;
+          : op === "And"
+          ? true
+          : false;
       },
-      'Contains': function(row) {
+      Contains: function(row) {
         return filterValues.filter2Value
-          ? row[column]
+          ? propf(prop, row)
               .toLowerCase()
               .includes(filterValues.filter2Value.toLowerCase())
-          : op==="And"?true :false;
+          : op === "And"
+          ? true
+          : false;
       },
       "Does not contain": function(row) {
         return filterValues.filter2Value
-          ? !row[column]
+          ? !propf(prop, row)
               .toLowerCase()
               .includes(filterValues.filter2Value.toLowerCase())
-          : op==="And"?true :false;
+          : op === "And"
+          ? true
+          : false;
       },
       "Ends with": function(row) {
         return filterValues.filter2Value
-          ? row[column]
+          ? propf(prop, row)
               .toLowerCase()
               .endsWith(filterValues.filter2Value.toLowerCase())
-          : op==="And"?true :false;
+          : op === "And"
+          ? true
+          : false;
       }
     };
     return values[filterBy](row);
   };
 
-  const  operand1= (filterBy:string, row:Column):boolean => {
+  const operand1 = (filterBy: FilterOptions, row: Column): boolean => {
     let values = {
       "Is equal to": function(row) {
         return (
-          row[column].toLowerCase() === filterValues.filter1Value.toLowerCase()
+          propf(prop, row).toLowerCase() ===
+          filterValues.filter1Value.toLowerCase()
         );
       },
       "Is not equal to": function(row) {
         return (
-          row[column].toLowerCase() !== filterValues.filter1Value.toLowerCase()
+          propf(prop, row).toLowerCase() !==
+          filterValues.filter1Value.toLowerCase()
         );
       },
       "Starts with": function(row) {
-        return row[column]
+        return propf(prop, row)
           .toLowerCase()
           .startsWith(filterValues.filter1Value.toLowerCase());
       },
-      'Contains': function(row) {
-        return row[column]
+      Contains: function(row) {
+        return propf(prop, row)
           .toLowerCase()
           .includes(filterValues.filter1Value.toLowerCase());
       },
       "Does not contain": function(row) {
-        return !row[column]
+        return !propf(prop, row)
           .toLowerCase()
           .includes(filterValues.filter1Value.toLowerCase());
       },
       "Ends with": function(row) {
-        return row[column]
+        return propf(prop, row)
           .toLowerCase()
           .endsWith(filterValues.filter1Value.toLowerCase());
       }
@@ -117,12 +132,13 @@ const filterRows = (
   };
 
   return rowsToFilter.filter(row =>
-     op(
+    op(
       operand1(filterValues.filter1By, row),
       filterValues.compareValue,
-      operand2(filterValues.filter2By, row,filterValues.compareValue)
+      operand2(filterValues.filter2By, row, filterValues.compareValue)
     )
   );
 };
 
 module.exports = filterRows;
+
